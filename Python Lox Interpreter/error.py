d0b4691a9d941a7676d77c5c1df61bf310f24b76
jvillacorte <joseph.villacorte@gmail.com>
2026-03-24 02:27:20 -0700

    code comments

--- Python Lox Interpreter/error.py
+++ Python Lox Interpreter/error.py
@@ -2,23 +2,29 @@ from sys import stderr
 
 had_err = False
 
+#runtime failures when executing code
+#stores tiken where runtime problem happened and message describing error
 class RuntimeErr(Exception):
     def __init__(self, token, message: str) -> None:
         self.token = token
         self.message = message
         super().__init__(self.message)
 
-
+#parsing failures when parsing code
+#dont do nothing
 class ParseErr(Exception):
     pass
 
+#reports error to stderr, sets error status to true, used for both runtime and parse errors
 def set_err_status(value: bool) -> None:
     global had_err
     had_err = value
 
+#reads global had_err flag, checks if error has occured after scan/parse/interpret stages to determine if program should exit with error code
 def get_err_status() -> bool:
     return had_err
 
+#reports error to stderr, sets error status to true, used for both runtime and parse errors
 def report(line: int, where: str, message: str) -> None:
     print(f'[line {line}] Error{where}: {message}', file=stderr)
     set_err_status(True)
